7f8c0fef04955a1920570a23b1d59c49e4ee6149
yuhuan <597270222@qq.com>
2024-04-26 17:11:03 +0800

    week2-作业：02-线性结构4 Pop Sequence

--- src/week2/Main3.java
+++ src/week2/Main3.java
@@ -20,6 +20,16 @@ a.arr[index1]>栈顶数据,则另itemEnd = arr[index1];将itemStart、itemEnd之
 b.arr[index1]==栈顶数据，pop栈顶数据，index1++，继续比较下一项
 c.arr[index1]<栈顶数据,说明形成不了这样的序列，no，跳出循环
 
+作业地址：https://pintia.cn/problem-sets/1738108464136978432/exam/problems/type/7?problemSetProblemId=1738108464208281607&page=0
+
+Given a stack which can keep M numbers at most. Push N numbers in the order of 1, 2, 3, ..., N and pop randomly. You are supposed to tell if a given sequence of numbers is a possible pop sequence of the stack. For example, if M is 5 and N is 7, we can obtain 1, 2, 3, 4, 5, 6, 7 from the stack, but not 3, 2, 1, 7, 5, 6, 4.
+
+Input Specification:
+Each input file contains one test case. For each case, the first line contains 3 numbers (all no more than 1000): M (the maximum capacity of the stack), N (the length of push sequence), and K (the number of pop sequences to be checked). Then K lines follow, each contains a pop sequence of N numbers. All the numbers in a line are separated by a space.
+
+Output Specification:
+For each pop sequence, print in one line "YES" if it is indeed a possible pop sequence of the stack, or "NO" if not.
+
 Sample Input:
 5 7 5
 1 2 3 4 5 6 7
